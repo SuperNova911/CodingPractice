acb1943bd8c99f1a3e8bed5db869125c120f433a
Suwhan Kim <suwhan77@naver.com>
2021-03-17 01:30:43 +0900

    2021 KAKAO BLIND RECRUITMENT: 신규 아이디 추천 풀이
    정규식을 활용한 풀이

---
+++ Programmers/2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천/신규 아이디 추천/main.swift
@@ -0,0 +1,51 @@
+//
+//  main.swift
+//  신규 아이디 추천
+//
+//  Created by 김수환 on 2021/03/17.
+//
+
+import Foundation
+
+func solution(_ newID:String) -> String {
+    // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+    var newID = newID.lowercased()
+    
+    // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+    newID = newID.replacingOccurrences(of: "[^a-z0-9-_.]", with: "", options: .regularExpression)
+    
+    // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+    newID = newID.replacingOccurrences(of: #"\.+"#, with: ".", options: .regularExpression)
+    
+    // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    newID =  newID.replacingOccurrences(of: #"^\.|\.$"#, with: "", options: .regularExpression)
+    
+    // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if newID.isEmpty {
+        newID.append("a")
+    }
+    
+    // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+    newID = String(newID.prefix(15))
+    // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+    newID =  newID.replacingOccurrences(of: #"\.$"#, with: "", options: .regularExpression)
+    
+    // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+    while newID.count < 3 {
+        newID.append(newID.last!)
+    }
+    
+    return newID
+}
+
+let newID1 = "...!@BaT#*..y.abcdefghijklm"
+let newID2 = "z-+.^."
+let newID3 = "=.="
+let newID4 = "123_.def"
+let newID5 = "abcdefghijklmn.p"
+
+print(solution(newID1))     // "bat.y.abcdefghi"
+print(solution(newID2))     // "z--"
+print(solution(newID3))     // "aaa"
+print(solution(newID4))     // "123_.def"
+print(solution(newID5))     // "abcdefghijklmn"
